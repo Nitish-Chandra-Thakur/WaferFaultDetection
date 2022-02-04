891f2778b00bf3657a00a6582387d043fcf4158f
Nitish Chandra Thakur <nitishch.thakur@gmail.com>
2022-02-05 02:01:31 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wafer-fault-detection",
+    version="0.0.1",
+    description="ML project",
+    author="Nitish Chandra Thakur",
+    packages=find_packages(),
+    license="MIT"
+)
